6d5949fd2200a73f828554816db038a122af3fb7
unknown <birajjyo2@gmail.com>
2023-05-31 19:06:27 +0530

    first commit

--- confic/mongoose.js
+++ confic/mongoose.js
@@ -1,5 +1,12 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://0.0.0.0:27017/placement');
+// mongoose.connect('mongodb://0.0.0.0:27017/placement');
+const DB = 'mongodb+srv://birajjyo2:Od7oLjIdJVSCu4ps@cluster0.kybvwzo.mongodb.net/mernstack?retryWrites=true&w=majority';
+
+mongoose.connect(DB, {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
 const db = mongoose.connection;
 // error
 db.on('error',console.error.bind(console,'erroe connecting to db'));
